ddab8805ecd56631f530c33259584ebb236d0b07
hnjee <leehnjee@gmail.com>
2026-08-28 23:44:56 +0900

    solve: week2-1-01-string.py (closes #6)

--- week2/1. basic/01_string.py
+++ week2/1. basic/01_string.py
@@ -34,17 +34,32 @@ def is_palindrome(s):
     Returns:
         bool: 회문이면 True, 아니면 False
     """
+
     # TODO: 알파벳과 숫자만 남기고 소문자로 변환하세요
     # 힌트: isalnum() 메서드와 lower() 메서드 사용
-    pass
-    
+    s_list = [ch for ch in s if ch.isdigit() or ch.isalpha()]
+    final_s = "".join(s_list).lower()
+
     # TODO: 정제된 문자열이 회문인지 확인하세요
     # 방법1: 문자열을 뒤집어서 비교 ([::-1] 사용)
     # 방법2: 양 끝 인덱스를 이용한 투 포인터 방식
-    pass
     
-    #return False
+    #reversed_s = "".join(reversed(final_s))  #방법0
+    #reversed_s = final_s[::-1]  #방법1
+    '''
+    if final_s == reversed_s:
+        return True
+    return False
+    '''
 
+    #방법2: 새 문자열을 생성하지 않아서 공간복잡도적으로 좋음 
+    s_len = len(final_s)
+    for i in range(int(s_len/2)):
+        if final_s[i] != final_s[s_len-i-1]:
+            return False
+
+    return True
+            
 # 테스트 케이스
 if __name__ == "__main__":
     # 테스트 케이스 1
