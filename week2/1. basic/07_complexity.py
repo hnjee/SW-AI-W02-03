256fba57639742f456e3305664440322246bad8d
hnjee <leehnjee@gmail.com>
2026-09-02 15:16:53 +0900

    solve: week2-1-07-complexity (closes #12)

--- week2/1. basic/07_complexity.py
+++ week2/1. basic/07_complexity.py
@@ -34,8 +34,10 @@ def find_duplicates_brute_force(nums):
     # TODO: 이중 반복문으로 중복 찾기
     ## i번째 원소와 i+1 이후의 모든 원소를 비교
     ## 같은 원소를 찾으면 duplicates에 추가 (중복 추가 방지 필요)
-    pass
-    
+    for i in range(n):
+        for j in range(i+1, n):
+            if nums[i] == nums[j] and nums[i] not in duplicates:
+                duplicates.append(nums[i])
     return duplicates
 
 def find_duplicates_sorting(nums):
@@ -48,14 +50,16 @@ def find_duplicates_sorting(nums):
         return []
     
     # TODO: 배열을 정렬하세요 (nums.sort() 사용)
-    pass
+    nums.sort()
     
     duplicates = []
     
     # TODO: 인접한 원소를 비교하여 중복 찾기
     # i와 i+1 원소가 같고, duplicates에 없으면 추가
-    pass
-    
+    for i in range(len(nums)-1): 
+        if nums[i] == nums[i+1] and (nums[i] not in duplicates):
+            duplicates.append(nums[i])
+
     return duplicates
 
 def find_duplicates_hash(nums):
@@ -64,13 +68,19 @@ def find_duplicates_hash(nums):
     시간 복잡도: O(n)
     공간 복잡도: O(n)
     """
-    seen = set()
-    duplicates = set()
+
+    # [4, 3, 2, 7, 8, 2, 2, 3, 1]
+    seen = set() # 4, 3, 2, 7, 8 
+    duplicates = set() # 2, 3 
     
     # TODO: 각 원소를 순회하면서
     ## 이미 seen에 있으면 duplicates에 추가
     ## 없으면 seen에 추가
-    pass
+    for i in range(len(nums)):
+        if nums[i] not in seen:
+            seen.add(nums[i])
+        else: 
+            duplicates.add(nums[i])
     
     return list(duplicates)
 
